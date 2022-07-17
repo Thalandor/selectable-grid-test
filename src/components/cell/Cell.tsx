688a6ad16ffd3ce5d6f31b3d4f5b6ddaae55a027
IvanBV <3891584+Thalandor@users.noreply.github.com>
2022-07-18 01:34:10 +0200

    Add first version double click

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
+import { useCellContext } from '../../hooks/useCellContext';
+import { useDoubleClick } from '../../hooks/useDoubleClick';
 import styles from './Cell.module.scss';
 
-const Cell = () => {
+interface IProps {
+  // updateColumn: (color: any, column: number) => void;
+  column: number;
+}
+
+const Cell: React.FC<IProps> = ({ column }) => {
+  // STATE
   const [selected, setSelected] = useState(false);
+
+  // CUSTOM HOOKS
+  const { updateColumn, setUpdateColumn } = useCellContext();
+  const onClickHandler = useDoubleClick({
+    onClickHandler: () => setSelected(!selected),
+    onDoubleClickHandler: () => updateColumn('color', column)
+  });
+
   return (
     <div
       className={`${styles.cell} ${selected && styles.selected}`}
-      onClick={() => setSelected(!selected)}
-      onDoubleClick={() => console.log('double click!!!')}
+      onClick={onClickHandler}
     />
   );
 };
---
+++ src/hooks/useDoubleClick.ts
@@ -0,0 +1,35 @@
+import { useCallback, useRef } from 'react';
+
+interface IProps {
+  onClickHandler: (e: any) => void;
+  onDoubleClickHandler: (e: any) => void;
+}
+
+export const useDoubleClick = ({
+  onClickHandler,
+  onDoubleClickHandler
+}: IProps) => {
+  const clickTimeout = useRef<any>();
+
+  const clearClickTimeout = () => {
+    if (clickTimeout) {
+      clearTimeout(clickTimeout.current);
+      clickTimeout.current = null;
+    }
+  };
+
+  return useCallback(
+    (event: any) => {
+      clearClickTimeout();
+      if (onClickHandler && event.detail === 1) {
+        clickTimeout.current = setTimeout(() => {
+          onClickHandler(event);
+        }, 200);
+      }
+      if (event.detail % 2 === 0) {
+        onDoubleClickHandler(event);
+      }
+    },
+    [onClickHandler, onDoubleClickHandler]
+  );
+};
