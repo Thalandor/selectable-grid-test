acde60124c16f618e5bb1d71d8d544f9dcd2f3fb
IvanBV <3891584+Thalandor@users.noreply.github.com>
2022-07-18 01:34:38 +0200

    Add provider

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,10 +1,17 @@
+import CellProvider from '../../context/CellProvider';
 import Cell from '../cell/Cell';
 
 const Grid = () => {
   const rows: JSX.Element[] = [];
   const columns: JSX.Element[] = [];
+
+  const onDoubleClickHandler = (color: any, column: number) => {};
   for (let column = 0; column < (process.env.REACT_APP_LENGTH || 1); ++column) {
-    columns.push(<Cell />);
+    columns.push(
+      <CellProvider>
+        <Cell column={column} />
+      </CellProvider>
+    );
   }
   for (let row = 0; row < (process.env.REACT_APP_HEIGHT || 1); ++row) {
     rows.push(<div style={{ display: 'flex' }}>{columns}</div>);
